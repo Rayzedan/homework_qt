0ed5b29698e1bc408c4514ab1bb6c0c4654c8acf
Daniil Bezlepkin <bezlepkin_d01@mail.ru>
2023-04-03 13:27:29 +0500

    added compile cmake flags and fix bugs with buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,41 +3,39 @@
 #include <QTimer>
 #include <QPushButton>
 #include <QDebug>
-#include <QPalette>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    QPalette color;
-    color.setColor(QPalette::Window, Qt::red);
-    setCentralWidget(new QWidget(this));
+    setWindowTitle("Game");
 
     QTimer *timer = new QTimer(this);
-
-    connect(timer,&QTimer::timeout,[this, timer, color](){
-        QPushButton *button = new QPushButton(this);
-        QTimer *buttonTimer = new QTimer(this);
-
+    connect(timer, &QTimer::timeout, [this, timer](){
+        QPushButton* button = new QPushButton("*", this);
+        QTimer* buttonTimer = new QTimer(this);
         const int x = QRandomGenerator::global()->bounded(0,width());
         const int y = QRandomGenerator::global()->bounded(0,100);
         const int speed = QRandomGenerator::global()->bounded(1,5);
 
         button->setFixedSize(25,25);
 
-        connect(buttonTimer, &QTimer::timeout, [this, button, speed, color]() {
+        connect(button, &QPushButton::clicked, this, [button, buttonTimer]() {
+            button->deleteLater();
+            buttonTimer->stop();
+        });
+
+        connect(buttonTimer, &QTimer::timeout, this, [this, button, speed]() {
             if (button->underMouse())
                 button->move(button->x(), button->y() + 2*speed);
             else
                 button->move(button->x(), button->y() + speed);
 
             if (button->y() + 25 >= height()) {
+                button->deleteLater();
                 setWindowTitle("YOU LOSE!");
-                setPalette(color);
+                setStyleSheet("background-color: red;");
             }
-        });
-
-        connect(button, &QPushButton::clicked,[button]{
-            button->close();
         });
 
         button->setGeometry(x, y, button->width(), button->height());
